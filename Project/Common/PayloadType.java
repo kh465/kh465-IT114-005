3b7b7bac85e174fe5626eff00750d4e067b9e396
Keven Henriques <uwu@owobook-2.local>
2024-12-11 17:17:34 -0500

    completed tasks for milestone4

---
+++ Project/Common/PayloadType.java
@@ -0,0 +1,17 @@
+package Project.Common;
+
+public enum PayloadType {
+    CLIENT_CONNECT, // client requesting to connect to server (passing of initialization data [name])
+    CLIENT_ID,  // server sending client id
+    SYNC_CLIENT,  // silent syncing of clients in room
+    DISCONNECT,  // distinct disconnect action
+    ROOM_CREATE,
+    ROOM_JOIN, // join/leave room based on boolean
+    MESSAGE, // sender and message,
+    ROOM_LIST, // client: query for rooms, server: result of query
+    ROLL, //issues a roll command
+    FLIP, //issues a flip command
+    DM, //intended recipient, sender and message
+    MUTE, //mutes named client to no longer receive messages
+    UNMUTE //unmutes named client to receive messages
+}
